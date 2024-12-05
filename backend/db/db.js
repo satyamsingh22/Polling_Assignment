2fb0c06634349a4674f57e1aaf7d8f5ea0347525
Satyam Singh <116868686+satyamsingh22@users.noreply.github.com>
2024-12-06 01:22:39 +0530

    kafka

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,6 +3,7 @@ import { Sequelize } from 'sequelize';
 const sequelize = new Sequelize('Pollying', 'postgres', 'satyam2203', {
   host: 'localhost',
   dialect: 'postgres',
+  port: 5432,
 });
 
 export default sequelize;
